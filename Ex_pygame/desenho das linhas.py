374c4cff1961b548a0c6c3b3b4f3114022d304ba
Lipessousa <88160166+Lipessousa@users.noreply.github.com>
2021-12-02 16:43:59 -0300

    Add files via upload

---
+++ Ex_pygame/desenho das linhas.py
@@ -0,0 +1,10 @@
+#desenho das linhas
+
+linha 1= pygame.draw.line(tela, branco, (100, 0), (100, 600), 5)
+coluna 1= pygame.draw.line(tela, branco, (0, 100), (600, 100), 5)
+
+linha 2= pygame.draw.line(tela, branco, (300, 0), (300, 600), 5)
+coluna 2= pygame.draw.line(tela, branco, (0, 300), (600, 300), 5)
+
+linha 3= pygame.draw.line(tela, branco, (500, 0), (500, 600), 5)
+coluna 3= pygame.draw.line(tela, branco, (0, 500), (600, 500), 5)
